52924955e654eefc255706ee8fc2f9172f5612f8
Murad <muradmustafayev03@gmail.com>
2023-09-19 20:52:15 +0400

    Update test_cv2.py

--- test_cv2.py
+++ test_cv2.py
@@ -3,21 +3,28 @@ import time
 import numpy as np
 import psutil
 
-cap = cv2.VideoCapture("input.mp4", apiPreference=cv2.CAP_FFMPEG)
-frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-start = time.perf_counter()
-while True:
-    ret, frame = cap.read()
-    if ret is False:
-        break
-    assert frame.shape == (2160, 3840, 3)
-    assert frame.dtype == np.uint8
-end = time.perf_counter()
+def test_cv2(input_video):
+    cap = cv2.VideoCapture(input_video, apiPreference=cv2.CAP_FFMPEG)
+    frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-memory_usage = psutil.Process().memory_info().rss / (1024 ** 2)
+    start = time.perf_counter()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+    end = time.perf_counter()
 
-print(f"{frames / (end - start):.1f} frames per second")
-print(f"Memory usage: {memory_usage:.2f} MB")
+    memory_usage = psutil.Process().memory_info().rss / (1024 ** 2)
+    fps = frames / (end - start)
 
-cap.release()
+    cap.release()
+
+    return fps, memory_usage
+
+
+if __name__ == '__main__':
+    for i in range(5):
+        fps, mem = test_cv2(f"input{i}.mp4")
+        print(f"FPS: {fps:.1f}")
+        print(f"Memory usage: {mem:.2f} MB")
